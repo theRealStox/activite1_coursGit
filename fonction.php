0c35058640c593668fed84e8198a393a6877b0da
Mana <stox.kroc@gmail.com>
2018-09-20 18:20:20 +0200

    Modification de fonction.php, amélioration de la fonction de date.

--- fonction.php
+++ fonction.php
@@ -1,6 +1,13 @@
 <?php
 
-function getDatetime() // retourne la date et l'heure de maintenant au format datetime
+function getDatetime($mod) // retourne la date et l'heure de maintenant au format datetime selon $mod
 {
-	$today = date('Y-m-d H:i:s');
+	if ($mod == 'datetimeEng')
+	{
+		$today = date('Y-m-d H:i:s');
+	}
+	elseif ($mod == 'datetimeFra')
+	{
+		$today = date('d/m/Y H:i:s');
+	}
 }
